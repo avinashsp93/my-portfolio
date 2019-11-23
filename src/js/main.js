23c1ecbfbaf9f501394e47c9bac769c4eec776d6
Avinash Sorab <avinashsp93@gmail.com>
2019-11-22 18:31:21 -0600

    scrollTo for all the sections

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,29 @@
-$("#anchor-skills span").click(function() {
-    console.log('click')
+// Scroll to events on click of nav buttons
+
+$("#anchor-home span").click(function() {
+    console.log($("#skills").offset());
     $('html, body').animate(
-        {scrollTop: $("#skills").offset().top}, 500
+        {scrollTop: 0}, 1500
+    )
+});
+
+$("#anchor-skills span").click(function() {
+    console.log($("#skills").offset());
+    $('html, body').animate(
+        {scrollTop: $("#skills").offset().top}, 1500
+    )
+});
+
+$("#anchor-projects span").click(function() {
+    console.log($("#projects").offset());
+    $('html, body').animate(
+        {scrollTop: $("#projects").offset().top}, 1500
+    )
+});
+
+$("#anchor-activities span").click(function() {
+    console.log($("#activities").offset());
+    $('html, body').animate(
+        {scrollTop: $("#activities").offset().top}, 1500
     )
 });
